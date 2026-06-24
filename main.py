f28ebbc9ec2d6a6addfc57c10efcba3bcaecf841
Nks-tech-ctrl <nikhilsingh110104@gmail.com>
2026-06-24 22:34:40 +0530

    create account creation/login

---
+++ main.py
@@ -0,0 +1,35 @@
+print("===========Expense-Tracker=========")
+while True:
+    print("1.Create Account")
+    print("2.Login ")
+    print("3.Exit")
+
+    choice = input("Create account or Login!: ")
+    if choice == "1":
+        userName = input("Enter UserName:")
+        password = input("Enter user password: ")
+
+        with open(
+            r"C:\Users\amars\OneDrive\Desktop\Expense Tracker\user.txt", "a"
+        ) as f:
+            f.write(userName + "," + password + "\n")
+        print("Account successfully")
+    elif choice == "2":
+        userName = input("Enter UserName:")
+        password = input("Enter user password:")
+        with open(
+            r"C:\Users\amars\OneDrive\Desktop\Expense Tracker\user.txt", "r"
+        ) as f:
+            for line in f:
+                line = line.strip()
+                if not line:
+                    continue
+                stored_user, stored_pass = line.split(",")
+
+                if userName == stored_user and password == stored_pass:
+                    print("Login Successfully")
+    elif choice == "3":
+        print("Exiting....")
+        break
+    else:
+        print("Invalid Choice!")
