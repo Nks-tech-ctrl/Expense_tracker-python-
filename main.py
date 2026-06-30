4fc3f23372bd81d66af368b341e7e9bfaf5b8e2d
Nks-tech-ctrl <nikhilsingh110104@gmail.com>
2026-06-30 09:47:18 +0530

    Refactor expense tracker into functions

--- main.py
+++ main.py
@@ -1,204 +1,233 @@
 import os
 
-print("===========Expense-Tracker=========")
-while True:
-    print("1.Create Account")
-    print("2.Login ")
-    print("3.Exit")
 
-    choice = input("Create account or Login!: ")
+def create_account():
+    userName = input("Enter UserName:")
+    password = input("Enter user password: ")
 
-    if choice == "1":
-        userName = input("Enter UserName:")
-        password = input("Enter user password: ")
+    with open(r"C:\Users\amars\OneDrive\Desktop\Expense Tracker\user.txt", "a") as f:
+        f.write(userName + "," + password + "\n")
 
-        with open(
-            r"C:\Users\amars\OneDrive\Desktop\Expense Tracker\user.txt", "a"
-        ) as f:
-            f.write(userName + "," + password + "\n")
-        print("Account successfully")
+    print("Account successfully")
 
-    elif choice == "2":
-        userName = input("Enter UserName:")
-        password = input("Enter user password:")
 
-        login_success = False
+def login():
+    userName = input("Enter UserName:")
+    password = input("Enter user password:")
 
-        with open(
-            r"C:\Users\amars\OneDrive\Desktop\Expense Tracker\user.txt", "r"
-        ) as f:
+    login_success = False
+
+    with open(r"C:\Users\amars\OneDrive\Desktop\Expense Tracker\user.txt", "r") as f:
+        for line in f:
+            line = line.strip()
+
+            if not line:
+                continue
+
+            stored_user, stored_pass = line.split(",")
+
+            if userName == stored_user and password == stored_pass:
+                login_success = True
+                break
+
+    if login_success:
+        print("\nLogin Successful")
+        print(f"\nWelcome {userName}")
+        expense_menu(userName)
+    else:
+        print("Invalid username or Password")
+
+
+def add_expense(userName):
+    Date = input("Enter expense date(DD/MM/YYYY):")
+    Category = input("Enter expense category:")
+    Amount = input("Enter expense amount: ")
+    Description = input("Enter expense description:")
+
+    expense_file = f"{userName}_expense.txt"
+    expense_id = 1
+
+    if os.path.exists(expense_file):
+        with open(expense_file, "r") as f:
+            expense_id = len(f.readlines()) + 1
+
+    with open(expense_file, "a") as f:
+        f.write(f"{expense_id},{Date},{Category},{Amount},{Description}\n")
+
+    print("Expense Added Successfully!")
+
+
+def view_expense(userName):
+    expense_file = f"{userName}_expense.txt"
+
+    try:
+        with open(expense_file, "r") as f:
+            print("\n" + "=" * 70)
+            print(f"{'ID':<5}{'Date':<15}{'Category':<15}{'Amount':<10}{'Description'}")
+            print("=" * 70)
+
             for line in f:
                 line = line.strip()
+
                 if not line:
                     continue
 
-                stored_user, stored_pass = line.split(",")
+                expense_id, Date, Category, Amount, Description = line.split(",")
 
-                if userName == stored_user and password == stored_pass:
-                    login_success = True
-                    break
+                print(
+                    f"{expense_id:<5}{Date:<15}{Category:<15}{Amount:<15}{Description}"
+                )
 
-            if login_success:
-                print("\nLoginj Successfull")
-                print(f"\nWelcome {userName}")
+            print("=" * 70)
 
-                while True:
-                    print("1.Add Expense")
-                    print("2.View Expense")
-                    print("3.Update Expense")
-                    print("4.Delete Expense")
-                    print("5.Total Expense")
-                    print("6.Logout")
+    except FileNotFoundError:
+        print("No expenses found!")
 
-                    expense_choice = input("Enter the choice: ")
 
-                    if expense_choice == "1":
-                        Date = input("Enter expense date(DD/MM/YYYY):")
-                        Category = input("Enter expense category:")
-                        Amount = input("Enter expense amount: ")
-                        Description = input("Enter expense description:")
+def update_expense(userName):
+    update_id = input("Enter ID to update expense: ")
+    expense_file = f"{userName}_expense.txt"
 
-                        expense_file = f"{userName}_expense.txt"
-                        expense_id = 1
+    update_lines = []
+    found = False
 
-                        if os.path.exists(expense_file):
-                            with open(expense_file, "r") as f:
-                                expense_id = len(f.readlines()) + 1
+    with open(expense_file, "r") as f:
+        for line in f:
+            line = line.strip()
 
-                        with open(expense_file, "a") as f:
-                            f.write(
-                                f"{expense_id},{Date},{Category},{Amount},{Description}\n"
-                            )
+            if not line:
+                continue
 
-                        print("Expense Added Successfully!")
+            expense_id, Date, Category, Amount, Description = line.split(",")
 
-                    elif expense_choice == "2":
-                        expense_file = f"{userName}_expense.txt"
-                        try:
-                            with open(expense_file, "r") as f:
-                                print("\n" + "=" * 70)
-                                print(
-                                    f"{'ID':<5}{'Date':<15}{'Category':<15}{'Amount':<10}{'Description'}"
-                                )
-                                print("=" * 70)
+            if expense_id == update_id:
+                print("\nEnter new details")
+                Date = input("Enter expense date(DD/MM/YYYY):")
+                Category = input("Enter expense category:")
+                Amount = input("Enter expense amount: ")
+                Description = input("Enter expense description:")
+                found = True
 
-                                for line in f:
-                                    line = line.strip()
+            update_lines.append(
+                f"{expense_id},{Date},{Category},{Amount},{Description}\n"
+            )
 
-                                    if not line:
-                                        continue
+    with open(expense_file, "w") as f:
+        f.writelines(update_lines)
 
-                                    expense_id, Date, Category, Amount, Description = (
-                                        line.split(",")
-                                    )
-
-                                    print(
-                                        f"{expense_id:<5}{Date:<15}{Category:<15}{Amount:<15}{Description}"
-                                    )
-
-                                print("=" * 70)
-
-                        except FileNotFoundError:
-                            print("No expenses found!")
-
-                    elif expense_choice == "3":
-                        update_id = input("Enter ID to update expense: ")
-                        expense_file = f"{userName}_expense.txt"
-                        update_lines = []
-                        found = False
-
-                        with open(expense_file, "r") as f:
-                            for line in f:
-                                line = line.strip()
-
-                                if not line:
-                                    continue
-                                expense_id, Date, Category, Amount, Description = (
-                                    line.split(",")
-                                )
-
-                                if expense_id == update_id:
-                                    print("\nEnter new details")
-
-                                    Date = input("Enter expense date(DD/MM/YYYY):")
-                                    Category = input("Enter expense category:")
-                                    Amount = input("Enter expense amount: ")
-                                    Description = input("Enter expense description:")
-
-                                    found = True
-                                update_lines.append(
-                                    f"{expense_id},{Date},{Category},{Amount},{Description}\n"
-                                )
-                            with open(expense_file, "w") as f:
-                                f.writelines(update_lines)
-                            if found:
-                                print("Expense update successfully")
-                            else:
-                                print("Expense id is invalid!")
-                            print("=" * 60)
-
-                    elif expense_choice == "4":
-
-                        expense_file = f"{userName}_expense.txt"
-
-                        dlt_id = input("Enter ID to delete expense: ")
-                        update_lines = []
-
-                        found = False
-
-                        with open(expense_file, "r") as f:
-                            for line in f:
-                                line = line.strip()
-
-                                if not line:
-                                    continue
-                                expense_id, Date, Category, Amount, Description = (
-                                    line.split(",")
-                                )
-
-                                if expense_id == dlt_id:
-                                    found = True
-                                    continue
-                                update_lines.append(
-                                    f"{expense_id},{Date},{Category},{Amount},{Description}\n"
-                                )
-                        with open(expense_file, "w") as f:
-                            f.writelines(update_lines)
-
-                        if found:
-                            print("Expemse deleted successfully")
-                        else:
-                            print("ID not found!")
-                        print("Delete Expense selected")
-                    elif expense_choice == "5":
-                        total = 0
-
-                        with open(expense_file, "r") as f:
-                            for line in f:
-                                line = line.strip()
-
-                                if not line:
-                                    continue
-
-                                expense_id, Date, Category, Amount, Description = (
-                                    line.strip().split(",")
-                                )
-                                Amount = int(Amount)
-                                total += Amount
-                        print("=" * 60)
-                        print("Total Expense:", total)
-                        print("=" * 60)
-
-                    elif expense_choice == "6":
-                        print("Logout!")
-                        break
-                    else:
-                        print("Invalid Choice!")
-            else:
-                print("Invalid username or Password")
-
-    elif choice == "3":
-        print("Exiting....")
-        break
+    if found:
+        print("Expense update successfully")
     else:
-        print("Invalid Choice!")
+        print("Expense id is invalid!")
+
+    print("=" * 60)
+
+
+def delete_expense(userName):
+    expense_file = f"{userName}_expense.txt"
+
+    dlt_id = input("Enter ID to delete expense: ")
+
+    update_lines = []
+    found = False
+
+    with open(expense_file, "r") as f:
+        for line in f:
+            line = line.strip()
+
+            if not line:
+                continue
+
+            expense_id, Date, Category, Amount, Description = line.split(",")
+
+            if expense_id == dlt_id:
+                found = True
+                continue
+
+            update_lines.append(
+                f"{expense_id},{Date},{Category},{Amount},{Description}\n"
+            )
+
+    with open(expense_file, "w") as f:
+        f.writelines(update_lines)
+
+    if found:
+        print("Expense deleted successfully")
+    else:
+        print("ID not found!")
+
+    print("Delete Expense selected")
+
+
+def total_expense(userName):
+    expense_file = f"{userName}_expense.txt"
+
+    total = 0
+
+    with open(expense_file, "r") as f:
+        for line in f:
+            line = line.strip()
+
+            if not line:
+                continue
+
+            expense_id, Date, Category, Amount, Description = line.split(",")
+
+            total += int(Amount)
+
+    print("=" * 60)
+    print("Total Expense:", total)
+    print("=" * 60)
+
+
+def expense_menu(userName):
+    while True:
+        print("1.Add Expense")
+        print("2.View Expense")
+        print("3.Update Expense")
+        print("4.Delete Expense")
+        print("5.Total Expense")
+        print("6.Logout")
+
+        expense_choice = input("Enter the choice: ")
+
+        if expense_choice == "1":
+            add_expense(userName)
+        elif expense_choice == "2":
+            view_expense(userName)
+        elif expense_choice == "3":
+            update_expense(userName)
+        elif expense_choice == "4":
+            delete_expense(userName)
+        elif expense_choice == "5":
+            total_expense(userName)
+        elif expense_choice == "6":
+            print("Logout!")
+            break
+        else:
+            print("Invalid Choice!")
+
+
+def main():
+    print("===========Expense-Tracker=========")
+
+    while True:
+        print("1.Create Account")
+        print("2.Login")
+        print("3.Exit")
+
+        choice = input("Create account or Login or Exit!: ")
+
+        if choice == "1":
+            create_account()
+        elif choice == "2":
+            login()
+        elif choice == "3":
+            print("Exiting....")
+            break
+        else:
+            print("Invalid Choice!")
+
+
+if __name__ == "__main__":
+    main()
